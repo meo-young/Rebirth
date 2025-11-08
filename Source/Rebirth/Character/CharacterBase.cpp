f52db2fe4c510c25332d5f4264c325515462c772
meo-young <meoyoung000@naver.com>
2025-11-08 16:36:39 +0900

    [Update] 캐릭터 이동 방식 변경
    - 항상 월드 기준 X축, Y축 방향으로 이동하도록 구현

--- Source/Rebirth/Character/CharacterBase.cpp
+++ Source/Rebirth/Character/CharacterBase.cpp
@@ -105,24 +105,16 @@ void ACharacterBase::DoMove(const FInputActionValue& Value)
 	// 입력 값을 2D 벡터로 변환합니다.
 	const FVector2D& MoveVector = Value.Get<FVector2D>();
 
-	// 카메라/컨트롤러 방향의 Yaw만 추출합니다.
-	const FRotator ControlRot = Controller->GetControlRotation();
-	const FRotator YawRot(0.f, ControlRot.Yaw, 0.f);
-
-	// 방향 벡터를 계산합니다.
-	const FVector Forward = FRotationMatrix(YawRot).GetUnitAxis(EAxis::X);
-	const FVector Right   = FRotationMatrix(YawRot).GetUnitAxis(EAxis::Y);
-
 	// 캐릭터를 전방으로 이동시킵니다.
 	if (!FMath::IsNearlyZero(MoveVector.X))
 	{
-		AddMovementInput(Forward, MoveVector.X);
+		AddMovementInput(FVector(1.0f, 0.0f, 0.0f), MoveVector.X);
 	}
 
 	// 캐릭터를 측면으로 이동시킵니다.
 	if (!FMath::IsNearlyZero(MoveVector.Y))
 	{
-		AddMovementInput(Right, MoveVector.Y);
+		AddMovementInput(FVector(0.0f, 1.0f, 0.0f), MoveVector.Y);
 	}
 }
 
